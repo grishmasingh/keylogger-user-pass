188a24fa08f26066edf2c0ed31c51335a5e95b70
Grishma Singh <117310733+grishmasingh@users.noreply.github.com>
2022-11-13 22:25:08 +0000

    Add files via upload

---
+++ KeyloggerProject/control.py
@@ -0,0 +1,49 @@
+#
+# from pynput.mouse import Controller
+# # from pynput.keyboard import Controller
+# #
+# #
+# # def controlMouse():
+# #     mouse = Controller()
+# #     mouse.position = (10, 20)
+# # # controlMouse()
+# #
+# # #calling the controller
+# # #control the mouse by changing its position
+# # #(left to right, top to bottom) from top-left of screen (0,0)
+# #
+# # def controlKeyboard():
+# #     keyboard = Controller()
+# #     keyboard.type("I changed your keyboard")
+# #
+# # # controlKeyboard()
+# #
+# # #calling the controller
+# # #control the keyboard by typing automatically
+# #
+# #
+# # #listen to mouse position
+# # from pynput.mouse import Listener
+#
+# def writetofile(x,y):
+#     print('Position of current mouse(0)'.format((x,y)))
+#
+# with Listener(on_move=writetofile) as l:
+#     l.join()
+#
+# # on_move : it calls the writetofile function when mouse moves in x and y format
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# # #Controlling your mouse
+# # listening to your mouse
+# # controlling your keyboard
+# # listening to your keyboard - for our keylogger
